88221e8178f9efa58736925e473cb27cf5853c46
dmitrined <dm.nedioglo@gmail.com>
2025-11-11 22:21:14 +0100

    ok

--- my-redux-app/src/components/Consultation/Practice4/dishes/DishApp.tsx
+++ my-redux-app/src/components/Consultation/Practice4/dishes/DishApp.tsx
@@ -2,11 +2,11 @@ import type { JSX } from "react";
 import DishesList from "./DishesList";
 import DischForm from "./DischForm";
 
-export default function DishApp(): JSX.Element{
+export default function DishApp(): JSX.Element {
   return (
     <div>
       <DischForm />
       <DishesList />
     </div>
-  )
+  );
 }
