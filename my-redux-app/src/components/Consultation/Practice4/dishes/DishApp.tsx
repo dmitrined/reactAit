a767ad2d9aace610f873193a101f9155c339c215
dmitrined <dm.nedioglo@gmail.com>
2025-11-12 19:33:22 +0100

    ok

--- my-redux-app/src/components/Consultation/Practice4/dishes/DishApp.tsx
+++ my-redux-app/src/components/Consultation/Practice4/dishes/DishApp.tsx
@@ -7,6 +7,27 @@ export default function DishApp(): JSX.Element {
     <div>
       <DishForm />
       <DishesList />
+      {/* Кнопка для GitHub кода */}
+      <a
+  style={{
+    display: 'inline-block',
+    padding: '10px 20px',
+    textDecoration: 'none', 
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: '16px',
+    cursor: 'pointer',
+    color: 'white',
+    backgroundColor: '#24292e', 
+    border: '2px solid #24292e',
+    borderRadius: '6px',
+    marginTop: '15px' 
+  }}
+  target="_blank"
+  href="https://github.com/dmitrined/reactAit/tree/main/my-redux-app/src/components/Consultation/Practice4/dishes"
+>
+  Посмотреть код этой страницы на GitHub
+</a>
     </div>
   );
 }
